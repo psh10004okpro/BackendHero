3845d43059064d17cdc8a895c9c959f06a993336
mindvridge <111107460+mindvridge@users.noreply.github.com>
2024-12-21 21:05:13 +0900

    a 스크립트 올림

---
+++ Assets/a.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class a : MonoBehaviour
+{
+    private int name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        name = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
